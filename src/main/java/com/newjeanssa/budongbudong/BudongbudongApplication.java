9d3ec1bd228fe742eb472957a097ea7ac7ce0ed7
신혜승 <hyeseung2000@gmail.com>
2023-11-03 14:21:30 +0900

    Initial commit

---
+++ src/main/java/com/newjeanssa/budongbudong/BudongbudongApplication.java
@@ -0,0 +1,13 @@
+package com.newjeanssa.budongbudong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudongbudongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudongbudongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newjeanssa/budongbudong/BudongbudongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newjeanssa.budongbudong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudongbudongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
