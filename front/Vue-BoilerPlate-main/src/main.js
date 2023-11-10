d94f1a8060945194ba3208054c0cc57dd4f89728
신혜승 <hyeseung2000@gmail.com>
2023-11-10 11:45:35 +0900

    ✨ Feat : 회원 API

--- front/Vue-BoilerPlate-main/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import "./assets/App.scss";
-import App from "./App.vue";
-import router from "./router";
-
-const app = createApp(App);
-
-app.use(router);
-app.mount("#app");
