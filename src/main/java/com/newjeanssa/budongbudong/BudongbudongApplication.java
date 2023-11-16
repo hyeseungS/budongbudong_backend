792168bb13df3db1b0f39a6ded950f74a71d38c5
hyeseungS <hyeseung2000@gmail.com>
2023-11-16 15:01:44 +0900

    🐛 Fix: headers CORS 에러

--- src/main/java/com/newjeanssa/budongbudong/BudongbudongApplication.java
+++ src/main/java/com/newjeanssa/budongbudong/BudongbudongApplication.java
@@ -3,12 +3,32 @@ package com.newjeanssa.budongbudong;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @MapperScan(basePackageClasses = BudongbudongApplication.class)
 @SpringBootApplication
 public class BudongbudongApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BudongbudongApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:9000")
+                        .allowedOriginPatterns("*")
+                        .allowedHeaders("*")
+                        .exposedHeaders("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
 }
